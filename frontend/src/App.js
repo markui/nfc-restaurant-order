0dec25e134d75690bb55c0dfcec7becb683531d6
Mark Kim <viking617617@gmail.com>
2018-05-05 23:28:58 +0900

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>NFC Restaurant Order System</h1>
+        {/* background-image */}
+        {/* Restaurant Info Card */}
+        {/* Restaurant Menu Tab */}
+        {/* Restaurant Menu List */}
+      </div>
+    );
+  }
+}
+
+export default App;
