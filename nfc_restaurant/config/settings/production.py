01b23327aa053c66e0fb150963cd66a0e333899a
Mark Kim <viking617617@gmail.com>
2018-05-27 14:15:28 +0900

    Backend: AWS S3 연결

--- nfc_restaurant/config/settings/production.py
+++ nfc_restaurant/config/settings/production.py
@@ -8,13 +8,39 @@ DATABASES = config_secret_production['django']['databases']
 
 # Allowed Hosts
 ALLOWED_HOSTS = [
-
+    'localhost',
+    '.elasticbeanstalk.com'
 ]
 
 
 # Installed Apps
 INSTALLED_APPS += [
-    'django_extensions'
+    'storages'
 ]
+
+# AWS S3
+# 1. IAM User Secret Info
+AWS_ACCESS_KEY_ID = config_secret_production['aws']['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = config_secret_production['aws']['AWS_SECRET_ACCESS_KEY']
+# 2. Bucket Info
+AWS_STORAGE_BUCKET_NAME = config_secret_production['aws']['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+S3_URL = f'http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# 3. Storage Folder Name - used in 'storage_backends.py'
+AWS_STATIC_LOCATION = 'static'
+AWS_PUBLIC_MEDIA_LOCATION = 'media/public'
+AWS_PRIVATE_MEDIA_LOCATION = 'media/private'
+# 4. Access Domain
+# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+# 5. Static/Media File Storage
+
+# AWS Storage File Name
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'media'
+
+# S3 FileStorage
+DEFAULT_FILE_STORAGE = 'config.storages.MediaStorage'
+STATICFILES_STORAGE = 'config.storages.StaticStorage'
 
 DEBUG = False
