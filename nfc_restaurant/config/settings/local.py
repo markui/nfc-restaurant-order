136ca4a9c59a6fd0767a09a630bcfea958ead252
Mark Kim <viking617617@gmail.com>
2018-05-27 12:05:06 +0900

    Backend: .ebignore 수정

--- nfc_restaurant/config/settings/local.py
+++ nfc_restaurant/config/settings/local.py
@@ -3,7 +3,7 @@ from .base import *
 config_secret_local = json.loads(open(CONFIG_SECRET_LOCAL_FILE).read())
 
 # Database
-# DATABASES = config_secret_local['django']['databases']
+DATABASES = config_secret_local['django']['databases']
 
 
 # Allowed Hosts
